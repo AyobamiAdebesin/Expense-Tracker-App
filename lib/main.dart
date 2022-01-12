d0358b341d5ab70141558c4dd6f2d6da631b1d9f
AyobamiAdebesin <froshofficial@gmail.com>
2022-01-12 08:56:46 +0100

    Added new files

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,6 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   final List<Transaction> _usertransactions = [
-<<<<<<< HEAD
     Transaction(
         id: 'tx1', title: 'New Laptop', date: DateTime.now(), amount: 200.00),
     Transaction(
@@ -44,31 +43,6 @@ class _MyAppState extends State<MyApp> {
         id: 'tx7', title: 'New Charger', date: DateTime.now(), amount: 80.00),
   ];
 
-  List<Transaction> get _recentTransactions {
-    return (_usertransactions.where((transaction) {
-      return transaction.date
-          .isAfter(DateTime.now().subtract(Duration(days: 7)));
-    }).toList());
-  }
-
-=======
-    Transaction(
-        id: 'tx1', title: 'New Laptop', date: DateTime.now(), amount: 200.00),
-    Transaction(
-        id: 'tx2', title: 'New Books', date: DateTime.now(), amount: 120.50),
-    Transaction(
-        id: 'tx3', title: 'New Courses', date: DateTime.now(), amount: 100.67),
-    Transaction(
-        id: 'tx4', title: 'New Shoes', date: DateTime.now(), amount: 150.20),
-    Transaction(
-        id: 'tx5', title: 'New Phone', date: DateTime.now(), amount: 90.40),
-    Transaction(
-        id: 'tx6', title: 'New Wifi', date: DateTime.now(), amount: 70.53),
-    Transaction(
-        id: 'tx7', title: 'New Charger', date: DateTime.now(), amount: 80.00),
-  ];
-
->>>>>>> f48a109c45a3f67d7539d6eaa9553c5c945f8fd7
   void _addNewTransaction(String txtitle, double txamount) {
     final newTxn = Transaction(
         id: DateTime.now().toString(),
@@ -84,22 +58,14 @@ class _MyAppState extends State<MyApp> {
     showModalBottomSheet(
       context: ctx,
       builder: (_) {
-<<<<<<< HEAD
-        return GestureDetector(
-          child: NewTransaction(_addNewTransaction),
-          onTap: () {},
-          behavior: HitTestBehavior.opaque,
-        );
-=======
         return NewTransaction(_addNewTransaction);
->>>>>>> f48a109c45a3f67d7539d6eaa9553c5c945f8fd7
       },
     );
   }
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
+
     return Scaffold(
       appBar: AppBar(
         actions: [
@@ -107,31 +73,6 @@ class _MyAppState extends State<MyApp> {
             onPressed: () => _startAddNewTransaction(context),
             icon: Icon(Icons.add),
           ),
-        ],
-        title: Text(
-          'Personal Expense Tracker',
-          style: TextStyle(
-              // fontFamily: 'Opensans',
-              ),
-        ),
-        backgroundColor: Theme.of(context).primaryColor,
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Chart(_recentTransactions),
-            TransactionList(_usertransactions),
-          ],
-=======
-    return MediaQuery(
-      data: MediaQueryData(),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(
-            actions: [
-              IconButton(
                 onPressed: () => _startAddNewTransaction(context),
                 icon: Icon(Icons.add),
               ),
@@ -165,7 +106,7 @@ class _MyAppState extends State<MyApp> {
             onPressed: () => _startAddNewTransaction(context),
             child: Icon(Icons.add),
           ),
->>>>>>> f48a109c45a3f67d7539d6eaa9553c5c945f8fd7
+
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
@@ -173,7 +114,6 @@ class _MyAppState extends State<MyApp> {
         elevation: 10,
         onPressed: () => _startAddNewTransaction(context),
         child: Icon(Icons.add),
-      ),
     );
   }
 }
